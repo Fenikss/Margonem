ddb658d159cf172dec4e13cd029303883799b1a1
Fenikss <brandonkotulski@interia.pl>
2016-03-20 19:31:01 +0100

    Fixed Status Position

--- game/game.php
+++ game/game.php
@@ -5,15 +5,9 @@
 		<script type="text/javascript" src="js/jquery-1.4.2.js"></script>
 		<script type="text/javascript" src="js/idz.js"></script>
 		<script type="text/javascript" src="js/system.js"></script>
-
-
-		
-		
 		
 		
 		<link rel="stylesheet" type="text/css" href="game.css">
-
-
 
 
 <div id="bpanel"></div>
@@ -23,50 +17,18 @@
 <div id="d"></div>
 
 
-		
-		
-		
-		
-		
-		
-		
-		
+<div id="objects">
+<div id="oMap" style="background-position: 
+<?php echo $poz_x; ?>px <?php echo $poz_y; ?>px; background-image: url('<?php echo $mapa['maps']; ?>');"></div>
 
-<div id="objects"><div style="background-position: <?php echo $poz_x; ?>px <?php echo $poz_y; ?>px; 
-background-image: 
-url('<?php echo $mapa['maps']; ?>');"
- id="oMap"></div>
-
-
-<div id="status"><?php echo "".$mapa['nazwa']." (".$player['x'].",".$player['y'].")"; ?></div>
-
-
-
-
-
-
-
-
-
+<div id="status">
+<?php echo "".$mapa['nazwa']." (".$player['x'].",".$player['y'].")"; ?></div>
 
 
 <div style="background-image: 
 url(<?php echo $player['avatar']; ?>);
  display: block; width: 32px; height: 48px; left: 240px; top: 240px; 
 background-position: 0px 0px; z-index: 29;" id="oHero"></div>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <script type='text/javascript'>
